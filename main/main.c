3cde7fddd47e27fc97990c07fa9f0d9bfea4514c
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-03-18 08:34:02 -0500

    feat(main/main.c): Light toggles on any number dialed, dialing broken on desktop headset

--- main/main.c
+++ main/main.c
@@ -1,9 +1,15 @@
+#include "credentials.h"
 #include "driver/gpio.h"
 #include "driver/hw_timer.h"
+#include "esp_event_loop.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
 #include <stdio.h>
+#include <string.h>
 
 #define LED_GPIO 4
 #define LED_GPIO_MASK (1ULL << GPIO_Pin_4)
@@ -22,6 +28,76 @@ volatile uint32_t prev_num_dialed = 0;
 volatile uint32_t new_dialed_flag = 0;
 
 static xQueueHandle gpio_evt_queue = NULL;
+
+static const char *TAG = "wifi station";
+
+static esp_err_t event_handler(void *ctx, system_event_t *event) {
+  switch (event->event_id) {
+  case SYSTEM_EVENT_STA_START:
+    ESP_LOGI(TAG, "Wi-Fi started");
+    esp_wifi_connect();
+    break;
+  case SYSTEM_EVENT_STA_CONNECTED:
+    ESP_LOGI(TAG, "Connected to AP");
+    break;
+  case SYSTEM_EVENT_STA_GOT_IP:
+    ESP_LOGI(TAG, "Got IP address");
+
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    ESP_LOGI(TAG, "Disconnected from AP");
+    esp_wifi_connect();
+    break;
+  default:
+    break;
+  }
+  return ESP_OK;
+}
+
+void http_post() {
+  // Perform HTTP POST request
+  esp_http_client_config_t config = {
+      .url = URL,
+      .method = HTTP_METHOD_POST,
+      .event_handler = NULL,
+      .buffer_size = 4096,
+      .timeout_ms = 10000,
+  };
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+  const char *post_data = "{\"entity_id\":\"light.desk_lamp_light\"}";
+  esp_http_client_set_post_field(client, post_data, strlen(post_data));
+  esp_http_client_set_header(client, "Content-Type", "application/json");
+  esp_http_client_set_header(client, "Authorization", HASS_TOKEN);
+  esp_err_t err = esp_http_client_perform(client);
+  if (err == ESP_OK) {
+    ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d",
+             esp_http_client_get_status_code(client),
+             esp_http_client_get_content_length(client));
+  } else {
+    ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+  }
+  esp_http_client_cleanup(client);
+}
+
+void wifi_init_sta() {
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = WIFI_SSID,
+              .password = WIFI_PASSWORD,
+          },
+  };
+  ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
 
 static void callback(void *arg) {
   if (prev_num_dialed == num_dialed) {
@@ -50,6 +126,7 @@ static void toggle_LED(void *arg) {
           prev_num_dialed = num_dialed;
           hw_timer_alarm_us(FINISHED_DIALING, false);
         }
+      } else {
       }
       prev_trigger = xTaskGetTickCount();
     }
@@ -73,9 +150,12 @@ void app_main() {
 
   hw_timer_init(callback, NULL);
 
+  wifi_init_sta();
+
   while (1) {
     vTaskDelay(100 / portTICK_RATE_MS);
     if (new_dialed_flag) {
+      http_post();
       printf("dialed %d\n", num_dialed);
       new_dialed_flag = 0;
     }
